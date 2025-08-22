ebc5ec3ac558c332aba55b6ffab5870e6b995608
AndreyKirov777 <gd@it-dynamics.ru>
2025-08-22 17:53:52 +0200

    20 lines in the table

--- src/components/RateCard.tsx
+++ src/components/RateCard.tsx
@@ -333,7 +333,7 @@ export function RateCard({ resources }: RateCardProps) {
           rowData={rowData}
           columnDefs={columnDefs}
           pagination={true}
-          paginationPageSize={10}
+          paginationPageSize={20}
           domLayout="autoHeight"
           suppressRowClickSelection={true}
           rowSelection="multiple"
